35276180a348466d5b74381d250aff21ea31c103
Deron <fambroderon@gmail.com>
2023-09-27 18:27:03 -0400

    added stying

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from "react";
 //we are importing these hooks that way we can use them
-// import 'src/Stopwatch.css'
+import "../StopWatch.css";
 
 const Stopwatch = () => {
   const [time, setTime] = useState(0); //seting the initial time to be zero
   const [isRunning, setIsRunning] = useState(false); //we are creating is running to represent if the watch is running or not and using the bool useState to represent the start and stop
 
-  useEffect(() => {  //using to set up the start stop
+  useEffect(() => {
+    //using to set up the start stop
     //using use effect to check if the timer is running
     let intervalId; //setting a intervalId
-    if (isRunning) {  //if the watch is running then the interval will update the Time state by 1 every 10 milisec
+    if (isRunning) {
+      //if the watch is running then the interval will update the Time state by 1 every 10 milisec
       intervalId = setInterval(() => setTime(time + 1), 10); //this intervalId will hold setTime as well as time. The setInterval is a function that is used that calls in milliseconds
     }
     return () => clearInterval(intervalId);
@@ -23,7 +25,7 @@ const Stopwatch = () => {
   //creating a functions to stop and start the timer
   const startAndStop = () => {
     //updating the isRunning to true
-    setIsRunning(!isRunning); 
+    setIsRunning(!isRunning);
   };
 
   //making a function to handle the reset of the timer
@@ -32,17 +34,33 @@ const Stopwatch = () => {
   };
 
   return (
-    <>
-    {/* converting the times to strings and using padStart to */}
-      {hours}:{min.toString().padStart(2, "0")}:
-      {sec.toString().padStart(2, "0")}:{millisec.toString().padStart(2, "0")}
-      <button id="start" onClick={startAndStop}>
-        {isRunning ? "Stop" : "Start"}
-      </button>
-      <button id="reset" onClick={reset}>
-        Reset
-      </button>
-    </>
+    <body>
+      <h1>I WANT TO PLAY A GAME</h1>
+        <img id="img" src="/5-old-tv-png-image-thumb.png" />
+      <div id="text">
+        <br></br>
+        <br></br>
+        <div>
+          {/* converting the times to strings and using padStart to */}
+          {hours}:{min.toString().padStart(2, "0")}:
+          {sec.toString().padStart(2, "0")}:
+          {millisec.toString().padStart(2, "0")}
+      </div>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <button id="start" onClick={startAndStop}>
+            {isRunning ? "Stop Game" : "Start Game"}
+          </button>
+          <button id="reset" onClick={reset}>
+            Reset
+          </button>
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
+    </body>
   );
 };
 
